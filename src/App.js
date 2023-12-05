20da9825c1f39498594648ea48b6f431af91cfa4
varshablog <shubhangikharat2910@gmail.com>
2023-12-05 12:13:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h2 className='text-3xl font-bold text-green-800'>Hello world</h2>
+    </div>
+  );
+}
+
+export default App;
